e9636b0ae12f36b5eb7290eb91f3af8012d0d157
Prabhat Kumar <pkkrsingh877@gmail.com>
2024-10-16 13:31:19 +0530

    Added Images, Projects Section Layout Done, Updated Hero Section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -6,9 +6,8 @@ const Hero = () => {
                 <h2 className="text-medium font-light p-2">A Passionate Web Developer</h2>
                 <p className="text-small font-normal">I specialize in creating beautiful and functional websites using the latest technologies. With a focus on user experience and responsive design, I strive to deliver seamless digital experiences that exceed expectations. Let's build something amazing together!</p>
             </div>
-            <div className="relative">
-                <img src="./images/profile-picture.png" alt="Prabhat Kumar's Profile Picture" className="w-[100%] rounded-[50%] hover:" />
-                <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 rounded-[50%] transition-opacity duration-300 flex justify-center items-center text-text font-extrabold text-large">Prabhat Kumar</div>
+            <div className="">
+                <img src="./images/profile-picture.png" alt="Prabhat Kumar's Profile Picture" className="w-[100%] rounded-[50%]" />
             </div>
         </section>
     );
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,6 +1,18 @@
 const Projects = () => {
     return (
-        <div>Projects</div>
+        <section className="flex flex-col p-4">
+            <h1 className="font-bold text-large text-center">Projects</h1>
+            <div className="grid grid-cols-3 gap-4">
+                <div className="h-[5rem] w-full">
+                    <h3 className="text-large">Mernstackblogs</h3>
+                </div>
+                <div className="h-[5rem] w-full"></div>
+                <div className="h-[5rem] w-full"></div>
+                <div className="h-[5rem] w-full"></div>
+                <div className="h-[5rem] w-full"></div>
+                <div className="h-[5rem] w-full"></div>
+            </div>
+        </section>
     );
 }
 
